e42223b5560caa68b800b51307711a66b4b34aeb
Amit Goswami <cseiubatgoswami@gmail.com>
2021-02-21 02:08:33 +0600

    urls_views

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
+from .models import Tweet
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return render(request, 'pages/home.html', context={})
