5757bae06a82f1561a558db0c9e6b5ad29db696b
Amit Goswami <cseiubatgoswami@gmail.com>
2021-02-21 05:06:06 +0600

    Like-button-rendaring

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 from .models import Tweet
@@ -15,7 +16,7 @@ def tweet_list_view(request, *args, **kwargs):
     tweet_list_view
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id, "content":x.content} for x in qs]
+    tweets_list = [{"id":x.id, "content":x.content, "likes":random.randint(0, 129) } for x in qs]
     data = {
         "is_user":False,
         "response": tweets_list
